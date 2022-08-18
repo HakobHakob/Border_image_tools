a831bbcf4a84e18cb57c8e290f5a0cb5f9bcb910
Hakob <hakobeg@gmail.com>
2022-08-18 12:41:39 +0400

    commit

--- src/BorderImageProperties/BorderImgWidth.js
+++ src/BorderImageProperties/BorderImgWidth.js
@@ -1,33 +1,35 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import * as Styled from "./Styled"
 import { CHECKBOX_LABELS, Items } from "./Constants"
 
 export const BorderImgWidth = () => {
-  const [isChekedAll, setIsCheckedAll] = useState(Items)
   const [checkedItems, setCheckedItems] = useState(Items)
 
-  const isAllSelected = Items.length > 0 && isChekedAll.length === Items.length
+  const CheckBoxRef = useRef(null)
 
-  const ToggleCheck = (e) => {
-    const value = e.target.value
+  const ToggleCheck = () => {
+    const copyCheckedItems = [...checkedItems]
 
-    if (value === "all") {
-      setIsCheckedAll(isChekedAll.length === Items.length ? [] : Items)
-    }
-    const list = [...isChekedAll]
-    const index = list.indexOf(value)
-
-    console.log(isChekedAll,'isCh')
-    console.log(index, "index")
-
-    index === -1 ? list.push(value) : list.splice(index, 1)
-    setIsCheckedAll(list)
+    setCheckedItems(
+      copyCheckedItems.map((item) =>
+        item.isChecked === false || item.isChecked === true
+          ? { ...item, isChecked: !item.isChecked }
+          : item
+      )
+    )
   }
+  useEffect(() => {
+    CheckBoxRef.current.setAttribute("checked", "")
+    CheckBoxRef.current.checked = checkedItems.isChecked
+    console.log(CheckBoxRef.current.checked, "24")
+  }, [checkedItems.isChecked])
+
+
 
   const CheckEdges = (labelName) => {
-    let clonedCheckedItems = [...checkedItems]
+    const cloneCheckedItems = [...checkedItems]
     setCheckedItems(
-      clonedCheckedItems.map((item) =>
+      cloneCheckedItems.map((item) =>
         item.name === labelName ? { ...item, isChecked: !item.isChecked } : item
       )
     )
@@ -41,8 +43,7 @@ export const BorderImgWidth = () => {
           <Styled.CheckBoxAll
             id="CheckAll"
             value="all"
-            onChange={ToggleCheck}
-            checked={isAllSelected}
+            onClick={ToggleCheck}
           />
           <label htmlFor="CheckAll">Check / Uncheck All</label>
         </Styled.Li>
@@ -54,9 +55,11 @@ export const BorderImgWidth = () => {
             <Styled.Li key={i}>
               <Styled.CheckBox
                 id={`custom-ceckbox-${i}`}
+                ref={CheckBoxRef}
                 name={labelName}
                 value={labelName}
-                onChange={() => CheckEdges(labelName)}
+                onClick={() => CheckEdges(labelName)}
+                defaultChecked={checkedItems.isChecked}
               ></Styled.CheckBox>
               <label htmlFor={`custom-ceckbox-${i}`}>{labelName}</label>
             </Styled.Li>
